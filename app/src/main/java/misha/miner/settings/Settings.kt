6e033f981ceabd366c985aa6d76f5a78628f0b46
artemyto <artemyto@yandex.ru>
2021-06-12 02:14:43 +0300

    Add command list

--- app/src/main/java/misha/miner/settings/Settings.kt
+++ app/src/main/java/misha/miner/settings/Settings.kt
@@ -2,7 +2,11 @@ package misha.miner.settings
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
@@ -23,6 +27,8 @@ fun Settings(settingsViewModel: SettingsViewModel) {
     var name by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
     var command by rememberSaveable { mutableStateOf("") }
+
+    val commandList by rememberSaveable { mutableStateOf(mutableListOf<String>()) }
 
     Column(modifier = Modifier
         .fillMaxHeight()
@@ -58,12 +64,33 @@ fun Settings(settingsViewModel: SettingsViewModel) {
             },
             label = { Text("Пароль") }
         )
-        TextField(value = command,
-            onValueChange = {
-                command = it
-            },
-            label = { Text("Добавить команду") }
-        )
+        Row {
+            TextField(value = command,
+                onValueChange = {
+                    command = it
+                },
+                label = { Text("Добавить команду") }
+            )
+            Button(onClick = {
+                commandList.add(command)
+                command = ""
+            }) {
+                Text("Add")
+            }
+        }
+        CommandList(items = commandList)
+    }
+}
+
+@Composable
+fun CommandList(
+    modifier: Modifier = Modifier,
+    items: List<String>
+) {
+    LazyColumn(modifier = modifier) {
+        itemsIndexed(items) { index, item ->
+            Text(item)
+        }
     }
 }
 
