c57eef5f1207a81d60c030668ab5771a5928980b
artemyto <artemyto@yandex.ru>
2021-06-13 10:56:23 +0300

    Add App class and init Realm

---
+++ app/src/main/java/misha/miner/App.kt
@@ -0,0 +1,22 @@
+package misha.miner
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Realm.init(this)
+
+        Realm.setDefaultConfiguration(
+            RealmConfiguration
+                .Builder()
+                .name("base.realm")
+                .allowWritesOnUiThread(true)
+                .build()
+        )
+    }
+}
