4e368ed66ea20d8c86987ef61ee1aba4ad0c18fb
artemyto <artemyto@yandex.ru>
2021-07-24 22:50:53 +0300

    Rewrite Home screen with ConstraintLayout

--- app/src/main/java/misha/miner/screens/home/Home.kt
+++ app/src/main/java/misha/miner/screens/home/Home.kt
@@ -1,19 +1,18 @@
 package misha.miner.screens.home
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import kotlinx.coroutines.Job
 import misha.miner.R
 
@@ -24,41 +23,60 @@ fun Home(viewModel: HomeViewModel, openDrawer: () -> Job) {
 
     val eth: String by viewModel.eth.observeAsState("")
     val poolOutputList: MutableList<String> by viewModel.poolOutputList.observeAsState(mutableListOf())
-    val balanceOutputList: MutableList<String> by viewModel.balanceOutputList.observeAsState(mutableListOf())
+    val balanceOutputList: MutableList<String> by viewModel.balanceOutputList.observeAsState(
+        mutableListOf()
+    )
 
-    Column(
-        modifier = Modifier
-            .fillMaxHeight()
-            .background(Color.LightGray)
-    ) {
+    ConstraintLayout(Modifier.fillMaxWidth()) {
 
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-            Button(onClick = {
+        val (menu, refresh, list) = createRefs()
+
+        Button(
+            onClick = {
                 openDrawer()
-            }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.img_burger_menu),
-                    contentDescription = null
-                )
+            },
+            modifier = Modifier.constrainAs(menu) {
+                top.linkTo(parent.top, margin = 16.dp)
+                start.linkTo(parent.start, margin = 16.dp)
             }
-            Button(onClick = { viewModel.runClicked() }) {
-                Text(text = "refresh")
-            }
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.img_burger_menu),
+                contentDescription = null
+            )
         }
-        Text(text = eth)
-        StringList(items = balanceOutputList)
-        StringList(items = poolOutputList)
-    }
-}
 
-@Composable
-fun StringList(
-    modifier: Modifier = Modifier,
-    items: List<String>
-) {
-    LazyColumn(modifier = modifier) {
-        itemsIndexed(items) { index, item ->
-            Text(item)
+        Button(
+            onClick = { viewModel.runClicked() },
+            modifier = Modifier.constrainAs(refresh) {
+                top.linkTo(parent.top, margin = 16.dp)
+                end.linkTo(parent.end, margin = 16.dp)
+                linkTo(menu.top, menu.bottom, topMargin = 0.dp, bottomMargin = 0.dp)
+                height = Dimension.fillToConstraints
+            }
+        ) {
+            Text(text = "refresh")
+        }
+        
+        LazyColumn(
+            modifier = Modifier.constrainAs(list) {
+                linkTo(parent.start, parent.end, startMargin = 16.dp, endMargin = 16.dp)
+                linkTo(menu.bottom, parent.bottom, topMargin = 16.dp, bias = 0F)
+                width = Dimension.fillToConstraints
+            }
+        ) {
+
+            item {
+                Text(eth)
+            }
+
+            items(balanceOutputList.size) { index ->
+                Text(balanceOutputList[index])
+            }
+
+            items(poolOutputList.size) { index ->
+                Text(poolOutputList[index])
+            }
         }
     }
 }
