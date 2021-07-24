9f0dee8a202d291fa9c16d7e750c3f011b077f9a
artemyto <artemyto@yandex.ru>
2021-07-24 23:24:05 +0300

    PCStats.kt: use BaseScreen()

--- app/src/main/java/misha/miner/screens/pc/PCStats.kt
+++ app/src/main/java/misha/miner/screens/pc/PCStats.kt
@@ -1,21 +1,17 @@
 package misha.miner.screens.pc
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.*
-import androidx.compose.runtime.getValue
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.Job
-import misha.miner.R
 import misha.miner.models.common.ErrorState
+import misha.miner.ui.common.BaseScreen
 
 @Composable
 fun PCStats(viewModel: PCStatsViewModel, openDrawer: () -> Job) {
@@ -27,28 +23,11 @@ fun PCStats(viewModel: PCStatsViewModel, openDrawer: () -> Job) {
 
     val outputList: MutableList<String> by viewModel.outputList.observeAsState(mutableListOf())
 
-    Column(
-        modifier = Modifier
-            .fillMaxHeight()
-            .background(Color.LightGray)
-    ) {
-
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-            Button(onClick = {
-                openDrawer()
-            }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.img_burger_menu),
-                    contentDescription = null
-                )
-            }
-            Button(onClick = { viewModel.runClicked() }) {
-                Text(text = "refresh")
-            }
-        }
-        Text(text = status)
-        StringList(items = outputList)
-    }
+    BaseScreen(
+        openDrawer = openDrawer,
+        refreshClicked = { viewModel.runClicked() },
+        list = mutableListOf(status).apply { addAll(outputList) }
+    )
 
     if (error is ErrorState.Error) {
         val message = (error as ErrorState.Error).message
@@ -76,17 +55,5 @@ fun PCStats(viewModel: PCStatsViewModel, openDrawer: () -> Job) {
                 }
             }
         )
-    }
-}
-
-@Composable
-fun StringList(
-    modifier: Modifier = Modifier,
-    items: List<String>
-) {
-    LazyColumn(modifier = modifier) {
-        itemsIndexed(items) { index, item ->
-            Text(item)
-        }
     }
 }
