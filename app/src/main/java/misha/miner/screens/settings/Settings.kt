97ba9a6e7fb02332ee364d120dc29293cde7b485
artemyto <artemyto@yandex.ru>
2021-07-25 02:36:08 +0300

    Change Settings

--- app/src/main/java/misha/miner/screens/settings/Settings.kt
+++ app/src/main/java/misha/miner/screens/settings/Settings.kt
@@ -1,6 +1,5 @@
 package misha.miner.screens.settings
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
@@ -9,12 +8,11 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import kotlinx.coroutines.Job
 import misha.miner.R
 import misha.miner.ui.common.ExposedDropDownMenu
@@ -35,96 +33,175 @@ fun Settings(viewModel: SettingsViewModel, openDrawer: () -> Job) {
     val commandList: MutableList<String> by viewModel.commandList.observeAsState(mutableListOf())
     val pcLabelList: MutableList<String> by viewModel.pcLabelList.observeAsState(mutableListOf())
 
-    Column(modifier = Modifier
-        .fillMaxHeight()
-        .background(Color.White)) {
+    ConstraintLayout(Modifier.fillMaxWidth()) {
 
-        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+        val (
+            menu,
+            refresh,
+            walletText,
+            commandDropDown,
+            pcDropDown,
+            add,
+            new,
+            remove,
+            addressText,
+            portText,
+            nameText,
+            passwordText
+        ) = createRefs()
 
-            Button(onClick = {
+        Button(
+            onClick = {
                 openDrawer()
-            }) {
-                Icon(painter = painterResource(id = R.drawable.img_burger_menu), contentDescription = null)
+            },
+            modifier = Modifier.constrainAs(menu) {
+                top.linkTo(parent.top, margin = 16.dp)
+                start.linkTo(parent.start, margin = 16.dp)
             }
-            Button(onClick = {
-                viewModel.save()
-            }) {
-                Text(text = "Сохранить")
-            }
-        }
-        Box(Modifier.padding(top = 16.dp, bottom = 8.dp)) {
-            TextField(value = wallet.value,
-                onValueChange = {
-                    viewModel.inputWallet(it)
-                },
-                label = { Text("Кошелёк") }
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.img_burger_menu),
+                contentDescription = null
             )
         }
-        Box(Modifier.padding(top = 8.dp, bottom = 8.dp)) {
-            Row(verticalAlignment = Alignment.CenterVertically,modifier = Modifier.fillMaxWidth()) {
-                ExposedDropDownMenu(
-                    label = { Text("Добавить команду") },
-                    suggestions = commandList,
-                    onTextChanged = {
-                        command = it
-                    },
-                    disableListAction = true
-                )
-                Button(
-                    onClick = {
-                    viewModel.addToCommandList(command)
-                    command = ""
-                }) {
-                    Text(text = "Add")
-                }
+
+        Button(
+            onClick = { viewModel.save() },
+            modifier = Modifier.constrainAs(refresh) {
+                top.linkTo(parent.top, margin = 16.dp)
+                end.linkTo(parent.end, margin = 16.dp)
+                linkTo(menu.top, menu.bottom, topMargin = 0.dp, bottomMargin = 0.dp)
+                height = Dimension.fillToConstraints
             }
+        ) {
+            Text(text = "save")
         }
-        Box(Modifier.padding(top = 8.dp, bottom = 16.dp)) {
-            Row(verticalAlignment = Alignment.CenterVertically,modifier = Modifier.fillMaxWidth()) {
-                ExposedDropDownMenu(
-                    label = { Text("Выбрать ПК") },
-                    text = pc,
-                    suggestions = pcLabelList,
-                    onTextChanged = {
-                        viewModel.selectedPC(it)
-                    }
-                )
-                Column {
-                    Button(
-                        onClick = {
-                            viewModel.newPC()
-                        }) {
-                        Text(text = "New pc")
-                    }
-                    Button(
-                        onClick = {
-                            viewModel.removePC()
-                        }) {
-                        Text(text = "Remove pc")
-                    }
-                }
+
+        TextField(
+            modifier = Modifier.constrainAs(walletText) {
+                top.linkTo(menu.bottom, 16.dp)
+                linkTo(parent.start, parent.end, 16.dp, 16.dp)
+                width = Dimension.fillToConstraints
+            },
+            value = wallet.value,
+            onValueChange = {
+                viewModel.inputWallet(it)
+            },
+            label = { Text("Кошелёк") }
+        )
+
+        ExposedDropDownMenu(
+            modifier = Modifier.constrainAs(commandDropDown) {
+                top.linkTo(walletText.bottom, 16.dp)
+                linkTo(parent.start, add.start, startMargin = 16.dp, endMargin = 16.dp)
+                width = Dimension.fillToConstraints
+            },
+            label = { Text("Добавить команду") },
+            suggestions = commandList,
+            onTextChanged = {
+                command = it
+            },
+            disableListAction = true
+        )
+
+        Button(
+            modifier = Modifier.constrainAs(add) {
+                top.linkTo(commandDropDown.top, 8.dp)
+                height = Dimension.value(56.dp)
+                end.linkTo(parent.end, 16.dp)
+            },
+            onClick = {
+                viewModel.addToCommandList(command)
+                command = ""
             }
+        ) {
+            Text(text = "add")
         }
+
+        ExposedDropDownMenu(
+            modifier = Modifier.constrainAs(pcDropDown) {
+                top.linkTo(add.bottom, 16.dp)
+                linkTo(parent.start, new.start, startMargin = 16.dp, endMargin = 16.dp)
+                width = Dimension.fillToConstraints
+            },
+            label = { Text("Выбрать ПК") },
+            text = pc,
+            suggestions = pcLabelList,
+            onTextChanged = {
+                viewModel.selectedPC(it)
+            }
+        )
+
+        Button(
+            modifier = Modifier.constrainAs(new) {
+                top.linkTo(pcDropDown.top, 8.dp)
+                height = Dimension.value(56.dp)
+                end.linkTo(remove.start, 16.dp)
+            },
+            onClick = {
+                viewModel.newPC()
+            }
+        ) {
+            Text(text = "New pc")
+        }
+
+        Button(
+            modifier = Modifier.constrainAs(remove) {
+                top.linkTo(pcDropDown.top, 8.dp)
+                height = Dimension.value(56.dp)
+                end.linkTo(parent.end, 16.dp)
+            },
+            onClick = {
+                viewModel.removePC()
+            }
+        ) {
+            Text(text = "Remove pc")
+        }
+
         if (pc.isNotBlank()) {
-            TextField(value = address,
+            TextField(
+                modifier = Modifier.constrainAs(addressText) {
+                    top.linkTo(new.bottom, 16.dp)
+                    linkTo(parent.start, parent.end, 16.dp, 16.dp)
+                    width = Dimension.fillToConstraints
+                },
+                value = address,
                 onValueChange = {
                     viewModel.inputAddress(it)
                 },
                 label = { Text("Адрес сервера") }
             )
-            TextField(value = port,
+            TextField(
+                modifier = Modifier.constrainAs(portText) {
+                    top.linkTo(addressText.bottom, 16.dp)
+                    linkTo(parent.start, parent.end, 16.dp, 16.dp)
+                    width = Dimension.fillToConstraints
+                },
+                value = port,
                 onValueChange = {
                     viewModel.inputPort(it)
                 },
                 label = { Text("Порт") }
             )
-            TextField(value = nameState.value,
+            TextField(
+                modifier = Modifier.constrainAs(nameText) {
+                    top.linkTo(portText.bottom, 16.dp)
+                    linkTo(parent.start, parent.end, 16.dp, 16.dp)
+                    width = Dimension.fillToConstraints
+                },
+                value = nameState.value,
                 onValueChange = {
                     viewModel.inputName(it)
                 },
                 label = { Text("Имя") }
             )
-            TextField(value = password,
+            TextField(
+                modifier = Modifier.constrainAs(passwordText) {
+                    top.linkTo(nameText.bottom, 16.dp)
+                    linkTo(parent.start, parent.end, 16.dp, 16.dp)
+                    width = Dimension.fillToConstraints
+                },
+                value = password,
                 onValueChange = {
                     viewModel.inputPassword(it)
                 },
