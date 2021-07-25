1c200d055fd03637813f3b06bb3edb51b2dbbc44
artemyto <artemyto@yandex.ru>
2021-07-25 04:22:50 +0300

    Add ArrowDropUp material icon

---
+++ app/src/main/java/misha/miner/ui/common/icons/ArrowDropUp.kt
@@ -0,0 +1,24 @@
+package misha.miner.ui.common.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.materialPath
+import androidx.compose.ui.graphics.vector.ImageVector
+
+val Icons.Filled.ArrowDropUp: ImageVector
+    get() {
+        if (_arrowDropUp != null) {
+            return _arrowDropUp!!
+        }
+        _arrowDropUp = materialIcon(name = "Filled.ArrowDropUp") {
+            materialPath {
+                moveTo(7.0f, 15.0f)
+                lineToRelative(5.0f, -5.0f)
+                lineToRelative(5.0f, 5.0f)
+                close()
+            }
+        }
+        return _arrowDropUp!!
+    }
+
+private var _arrowDropUp: ImageVector? = null
