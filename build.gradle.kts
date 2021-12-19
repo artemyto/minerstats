4d012a57168e6b80f304992d5c8c67657eaf9cba
artemyto <artemyto@yandex.ru>
2021-12-19 06:48:30 +0300

    Add Gradle Version Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 buildscript {
     val compose_version by extra("1.0.3")
     val hilt_version by extra ("2.39.1")
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
@@ -15,6 +17,24 @@ buildscript {
 
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
         classpath("io.realm:realm-gradle-plugin:10.8.0")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.39.0")
+    }
+}
+
+apply(plugin = "com.github.ben-manes.versions")
+
+fun String.isNonStable(): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(this)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+
+    // Example 2: disallow release candidates as upgradable versions from stable versions
+    rejectVersionIf {
+        candidate.version.isNonStable() && !currentVersion.isNonStable()
     }
 }
 
